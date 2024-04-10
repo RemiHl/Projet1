132be4c466fb69a7c44c3f05bd574a52feba248e
RemiHl <hlynski.r@gmail.com>
2024-04-10 17:25:02 +0200

    Removing and updating

--- Formulaire/script.js
+++ Formulaire/script.js
@@ -2,16 +2,16 @@ document.getElementById("formulaire").addEventListener("submit", function(event)
     event.preventDefault(); 
 
     // Récupérer les valeurs des champs
-    var title = document.getElementById("title").value;
-    var artist = document.getElementById("artist").value;
-    var genre = document.getElementById("genre").value;
-    var audioFile = document.getElementById("audio").files[0];
+    let titles = document.getElementById("title");
+    let artist = document.getElementById("artist");
+    let genre = document.getElementById("genre");
+    let audioFile = document.getElementById("audio");
 
     // Stockage local
-    localStorage.setItem('songData', JSON.stringify({ title: title, artist: artist, genre: genre}));
+    localStorage.setItem('songData', JSON.stringify({ title: titles, artist: artist, genre: genre}));
 
     // Stockage local fichier audio
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.onload = function(event) {
         localStorage.setItem('audioFile', event.target.result);
     };
--- Song/script.js
+++ Song/script.js
@@ -1,8 +1,38 @@
-// A voir pour le lecteur audio
+// Récupérer les données du formulaire
+let titles = document.getElementById("title");
+let artist = document.getElementById("artist");
+let genre = document.getElementById("genre");
+let audioFile = document.getElementById("audio");
 
+// Créer un nouvel élément liste
+let newItem = document.createElement("li");
+newItem.textContent = "Titre: " + titles + ", Artiste: " + artist + ", Genre: " + genre + ", Audio: " + audioFile;
 
-// var audioPlayer = document.getElementById('audioPlayer');
+// Créer un lecteur audio
+var audioPlayer = document.createElement("audio");
+audioPlayer.controls = true; // Ajoute les contrôles du lecteur audio
 
-// if (audioPlayer) {
-//     audioPlayer.remove();
-// }
+// Créer une source audio avec le fichier audio
+var source = document.createElement("source");
+console.log(newItem)
+source.src = URL.createObjectURL(audioFile); // Utilise le fichier audio entré dans le formulaire
+source.type = "audio/mp3"; // Spécifie le type de fichier audio 
+
+// Ajouter la source audio au lecteur
+audioPlayer.appendChild(source);
+
+// Ajouter le lecteur audio à l'élément de liste
+newItem.appendChild(audioPlayer);
+
+// Créer un bouton de suppression
+let deleteButton = document.createElement("button");
+deleteButton.textContent = "Supprimer";
+deleteButton.addEventListener("click", function() {
+    newItem.remove(); // Supprimer l'élément de la liste lors du clic sur le bouton
+    });
+
+// Ajouter le bouton de suppression à l'élément 
+newItem.appendChild(deleteButton);
+
+// Ajouter l'élément à la liste sur la page "Song"
+document.getElementById("songList").appendChild(newItem);
