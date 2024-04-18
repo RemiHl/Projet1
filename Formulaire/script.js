49c2c38099d973272fcd4b4083149dd3b3670cf3
RemiHl <hlynski.r@gmail.com>
2024-04-18 19:51:04 +0200

    Update Formulaire & Song

--- Formulaire/script.js
+++ Formulaire/script.js
@@ -2,13 +2,13 @@ document.getElementById("formulaire").addEventListener("submit", function(event)
     event.preventDefault(); 
 
     // Récupérer les valeurs des champs
-    let titles = document.getElementById("title");
-    let artist = document.getElementById("artist");
-    let genre = document.getElementById("genre");
-    let audioFile = document.getElementById("audio");
+    let title = document.getElementById("title").value;
+    let artist = document.getElementById("artist").value;
+    let genre = document.getElementById("genre").value;
+    let audioFile = document.getElementById("audio").files[0];
 
     // Stockage local
-    localStorage.setItem('songData', JSON.stringify({ title: titles, artist: artist, genre: genre}));
+    localStorage.setItem('songData', JSON.stringify({ title: title, artist: artist, genre: genre}));
 
     // Stockage local fichier audio
     let reader = new FileReader();
@@ -18,5 +18,35 @@ document.getElementById("formulaire").addEventListener("submit", function(event)
     reader.readAsDataURL(audioFile);
 
     // Redirection des données
-    window.location.href = "../Song/index.html";
+    window.location.href = "../Song/index.html"; 
 });
+
+
+
+
+
+
+
+
+
+// document.getElementById('audioInput').addEventListener('change', function(e) {
+//     console.log(e)
+//     const file = e.target.files[0];
+//     console.log(file)
+//     window.parent.postMessage({ file: file }, '*');
+// });
+
+// window.parent.postMessage({ file: inputFile.files[0] }, '*');
+
+
+
+// const audioInput = document.getElementById('audioInput');
+// const audioPlayer = document.getElementById('audioPlayer');
+
+// audioInput.addEventListener('change', (e) => {
+//     const file = e.target.files[0]
+//     const audioUrl = URL.createObjectURL(file)
+
+//     audioPlayer.src = audioUrl;
+//     audioPlayer.play();
+// })
--- Song/script.js
+++ Song/script.js
@@ -1,25 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Récupère les données stockées localement
-    var songData = JSON.parse(localStorage.getItem('songData'));
+    let songData = JSON.parse(localStorage.getItem('songData'));
+    let audioData = localStorage.getItem('audioFile');
 
-    // Affiche les données sur la page
-    if (songData) {
-        document.getElementById("title").textContent = songData.title;
-        document.getElementById("artist").textContent = songData.artist;
-        document.getElementById("genre").textContent = songData.genre;
+    if (songData && audioData) {
+        document.getElementById("songTitle").textContent = songData.title;
+        document.getElementById("songArtist").textContent = songData.artist;
+        document.getElementById("songGenre").textContent = songData.genre;
+
+        let audioPlayer = document.getElementById("audioPlayer");
+        audioPlayer.src = audioData;
+    } else {
+        console.log("Les données ne sont pas disponibles dans le localStorage.");
     }
 
-        //Evénements sur le bouton de suppression
-    var boutonSupprimer = document.getElementById("supprimerDonnees");
-    if (boutonSupprimer) {
-        boutonSupprimer.addEventListener("click", function() {
-
-        // Supprime les données stockées localement
+    document.getElementById("deleteDataButton").addEventListener("click", function () {
         localStorage.removeItem('songData');
-        
-        // Efface les données affichées sur la page
-        document.getElementById("title").textContent = "";
-        document.getElementById("artist").textContent = "";
-        document.getElementById("genre").textContent = "";
+        localStorage.removeItem('audioFile');
     });
-}});
+});
