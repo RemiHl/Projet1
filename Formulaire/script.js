3b517cd8be527fa931993a389542be46b2c2a4d4
RemiHl <hlynski.r@gmail.com>
2024-04-18 20:00:29 +0200

    Update Formulaire

--- Formulaire/script.js
+++ Formulaire/script.js
@@ -21,32 +21,3 @@ document.getElementById("formulaire").addEventListener("submit", function(event)
     window.location.href = "../Song/index.html"; 
 });
 
-
-
-
-
-
-
-
-
-// document.getElementById('audioInput').addEventListener('change', function(e) {
-//     console.log(e)
-//     const file = e.target.files[0];
-//     console.log(file)
-//     window.parent.postMessage({ file: file }, '*');
-// });
-
-// window.parent.postMessage({ file: inputFile.files[0] }, '*');
-
-
-
-// const audioInput = document.getElementById('audioInput');
-// const audioPlayer = document.getElementById('audioPlayer');
-
-// audioInput.addEventListener('change', (e) => {
-//     const file = e.target.files[0]
-//     const audioUrl = URL.createObjectURL(file)
-
-//     audioPlayer.src = audioUrl;
-//     audioPlayer.play();
-// })
