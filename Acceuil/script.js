ef7afa41d39028f827ddc31d502319c8b308cdd7
RemiHl <hlynski.r@gmail.com>
2024-05-28 14:20:48 +0200

    Ajout animation titre

--- Acceuil/script.js
+++ Acceuil/script.js
@@ -1,3 +1,10 @@
 document.getElementsByClassName("commencer").onclick = function() {
     window.location.href = "../Formuaire/index.html";
 }
+
+function blink() {
+    var title = document.getElementById('beatbench-title');
+    title.classList.toggle('blink');
+}
+
+setInterval(blink, 2000);
