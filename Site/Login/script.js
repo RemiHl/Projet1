7f24325888545dcfdddb0c13884cff67a385f351
RemiHl <hlynski.r@gmail.com>
2024-06-04 09:31:58 +0200

    Update script Login

--- Site/Login/script.js
+++ Site/Login/script.js
@@ -1,27 +1,3 @@
-// document.getElementById("loginForm").addEventListener("submit", function(event) {
-//     event.preventDefault();
-
-//     const username = document.getElementById("username").value;
-//     const password = document.getElementById("password").value;
-
-//     // Validation de l'utilisateur avec localStorage
-//     const storedUsername = localStorage.getItem("username");
-//     const storedPassword = localStorage.getItem("password");
-
-//     if (username === storedUsername && password === storedPassword) {
-//         document.getElementById("message").textContent = "Connexion réussie!";
-//         document.getElementById("message").style.color = "green";
-//         // Redirection après connexion réussie
-//         window.location.href = "../Acceuil/index.html";
-//     } else {
-//         document.getElementById("message").textContent = "Nom d'utilisateur ou mot de passe incorrect.";
-//     }
-// });
-
-// Login test (remplacer par une vraie gestion côté serveur)
-// localStorage.setItem("username", "testuser");
-// localStorage.setItem("password", "password123");
-
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
